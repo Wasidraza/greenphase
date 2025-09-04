a22e7770ce91a61ee1c71c296728bee3cf465f3a
Nashrakha <nashrak562@gmail.com>
2025-09-04 21:41:36 +0530

    add suspence

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,7 +1,11 @@
 "use client";
+import { Suspense } from "react";
 import Signup from "../Components/Signup";
 
-export const dynamic = "force-dynamic"; 
 export default function SignupPage() {
-  return <Signup />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Signup />
+    </Suspense>
+  );
 }
