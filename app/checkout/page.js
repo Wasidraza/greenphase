c6b7379250796b79cc85c4cc1198c5fd115a10ec
Nashrakha <nashrak562@gmail.com>
2025-09-04 22:03:25 +0530

    add force

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -3,6 +3,9 @@
 import { Suspense } from "react";
 import Checkout from "../Components/Checkout";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 export default function CheckoutPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
--- app/signup/page.js
+++ app/signup/page.js
@@ -2,6 +2,9 @@
 import { Suspense } from "react";
 import Signup from "../Components/Signup";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 export default function SignupPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
