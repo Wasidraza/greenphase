b8bba5f3ff00d4dcf6316911311780b8bcea91f1
Nashrakha <nashrak562@gmail.com>
2025-09-04 21:34:44 +0530

    add force-dynamic

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,11 +1,7 @@
 "use client";
-import { Suspense } from "react";
 import Signup from "../Components/Signup";
 
+export const dynamic = "force-dynamic"; 
 export default function SignupPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Signup />
-    </Suspense>
-  );
+  return <Signup />;
 }
