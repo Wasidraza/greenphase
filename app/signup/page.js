0acde22fdbed9efa2fdc8be56a743afe66fd96dc
Nashrakha <nashrak562@gmail.com>
2025-09-25 22:50:46 +0530

    solve signup

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,18 +1,7 @@
-// "use client";
-
-// import Signup from "../Components/Signup";
-
-// export default function SignupPage({ searchParams }) {
-
-//   const redirect = searchParams?.redirect || "/product/smart-home-charger";
-
-//   return <Signup redirect={redirect} />;
-// }
-
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import Signup from '../Components/Signup';
+import Signup from '@/app/Components/Signup';
 
 export default function SignupPage() {
   const searchParams = useSearchParams();
