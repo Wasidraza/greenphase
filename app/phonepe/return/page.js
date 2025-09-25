635f5b0456313867cacfa862c48599d9dd1d44d4
Nashrakha <nashrak562@gmail.com>
2025-09-25 22:24:15 +0530

    solve error

--- app/phonepe/return/page.js
+++ app/phonepe/return/page.js
@@ -1,18 +1,18 @@
+"use client";
+
 import dynamic from "next/dynamic";
 import Navbar from "@/app/Components/Navbar";
 import Footer from "@/app/Components/footer";
 
-const PaymentStatusClient = dynamic(
-  () => import("./PaymentStatus"),
-  { ssr: false }
-);
+const PaymentStatusClient = dynamic(() => import("./PaymentStatus"), {
+  ssr: false,
+});
 
 export default function PaymentReturn() {
   return (
     <>
       <Navbar />
-      <div className="p-8">
-        <h1>Payment Status</h1>
+      <div className="min-h-screen">
         <PaymentStatusClient />
       </div>
       <Footer />
